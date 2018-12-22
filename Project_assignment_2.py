1e5fe073ac1455770f04cae4d9c8e17d9a8328a1
yarzi01 <zy1000@gmail.com>
2018-12-22 17:30:49 +0200

    Update Project_assignment_2.py

--- Project_assignment_2.py
+++ Project_assignment_2.py
@@ -12,11 +12,9 @@ driver.get("http://192.168.99.102:5000/")              #open the webpage
 # driver.fullscreen_window()
 time.sleep(5)
 
-
 my_txt=driver.find_element_by_css_selector("body").text
 print(my_txt)
 
 driver.close()
-
 driver.quit()
 

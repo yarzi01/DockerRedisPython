24b6b666778c2f705f5fa65b0672d53f0cef0615
yarzi01 <zy1000@gmail.com>
2018-12-24 14:41:45 +0200

    Update Project_assignment_2.py

--- Project_assignment_2.py
+++ Project_assignment_2.py
@@ -8,13 +8,15 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(executable_path="/Users/yarzi01/Documents/DevOps/chromedriver")
-driver.get("http://192.168.99.102:5000/")              #open the webpage
+driver.get("http://localhost:5000/")              #open the webpage
 # driver.fullscreen_window()
 time.sleep(5)
 
 my_txt=driver.find_element_by_css_selector("body").text
-print(my_txt)
+print(smart_text(my_txt))
 
 driver.close()
 driver.quit()
+
+#end of program
 
